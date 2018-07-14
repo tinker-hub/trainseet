9144f039c75ab6dc6c2bac298ed09502f4e5379d
cmandocdoc <cedrickmandocdoc@gmail.com>
2018-07-14 19:19:46 +0800

    add services

--- app/services/train.service.js
+++ app/services/train.service.js
@@ -1,19 +1,19 @@
-// const bcryptService = require('../services/bcrypt.service');
-// const jwtService = require('../services/jsonwebtoken.service');
-// const Train = require('../models/train.model');
+const bcryptService = require('../services/bcrypt.service');
+const jwtService = require('../services/jsonwebtoken.service');
+const Train = require('../models/train.model');
 
-// class TrainService {
-//   async login(data) {
-//     const train = await Train.findOne({email: data.email});
-//     if (!train) throw new Error('Login failed');
-//     const isPasswordValid = await bcryptService.compareHash(data.password, train.password);
-//     if (!isPasswordValid) throw new Error('Login failed');
-//     const token = await jwtService.sign({
-//       _id: token._id,
-//       name: token.name
-//     });
-//     return { token: token, train: train };
-//   }
-// }
+class TrainService {
+  async login(data) {
+    const train = await Train.findOne({email: data.email});
+    if (!train) throw new Error('Login failed');
+    const isPasswordValid = await bcryptService.compareHash(data.password, train.password);
+    if (!isPasswordValid) throw new Error('Login failed');
+    const token = await jwtService.sign({
+      _id: token._id,
+      name: token.name
+    });
+    return { token: token, train: train };
+  }
+}
 
-// module.exports = new TrainService();
+module.exports = new TrainService();
