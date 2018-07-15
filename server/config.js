0b8729e4abb3ea5fa8f83a325c7b31c80935acae
cmandocdoc <cedrickmandocdoc@gmail.com>
2018-07-15 08:57:13 +0800

    updated config

--- server/config.js
+++ server/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   twitter: {
-    consumer_key: process.env.CONSUMER_KEY || 'LkSQyLF0euUgDUdGseEBctIsj',
-    consumer_secret: process.env.CONSUMER_SECRET || 'CnkLPhQJwwUnoSQSkq52DyDDeiU9zmY4DyOpHdT9fIGeZIaKdL',
-    access_token_key: process.env.ACCESS_TOKEN_KEY || '1018276019462029312-B25lf6iC9u1iajhBJD0FkC502mVbEc',
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET || 'ucmOTZaBxDNb97WClB0edSzs2dfmjmPbLa6NRoGljdNyG'
+    consumer_key: process.env.CONSUMER_KEY || 'aj3C4YAvhoZ8tyx211X20dOyZ',
+    consumer_secret: process.env.CONSUMER_SECRET || 'B43Vehn1m9m6qMERc7e70gzuPR8VzWIf9RYBCPbamNYCHiAExr',
+    access_token_key: process.env.ACCESS_TOKEN_KEY || '1018297745256222720-oRBxVHR3Uqyi8YAaiOi1QyyEQSnRIc',
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET || 'Jyb3PEXFgdhNzZ4nkmu9qcW5DfmlbyfkKeKKtVGkKj6ES'
   },
   streamTracks: [ // standard account can only stream one track
     // 'from:officialLRT1',
