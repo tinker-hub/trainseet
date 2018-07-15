c48689cd4a359d0ca2b0ddbb698c18e82c588695
cmandocdoc <cedrickmandocdoc@gmail.com>
2018-07-15 08:47:51 +0800

    wip

--- client/app.js
+++ client/app.js
@@ -1,17 +1,26 @@
-const url = 'http://localhost:8081';
+const url = 'http://178.128.63.0:8081/';
 const socket = io.connect(url);
 
 (async () => {
   const response = await fetch(`${url}/api/trains/`);
   const trains = await response.json();
-  const train = {
+  const train1 = {
     location: {
       coordinates: [120.980373, 14.6113278]
     },
     _id: trains[0]._id,
     speed: 7.5
   };
-  socket.emit('train', train);
+  const train2 = {
+    location: {
+      coordinates: [120.980373, 14.6113278]
+    },
+    _id: trains[0]._id,
+    speed: 7.5
+  };
+  socket.emit('train', train1);
+
+  socket.emit('train', train2);
   socket.on('eta', (data)=> {
     console.log(data);
   })
